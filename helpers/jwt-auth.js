aca5bb1ae82db07a070b79605931b20ed20a8fbd
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-01-28 14:53:19 -0300

    add jwt

---
+++ helpers/jwt-auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const generateJWT = (user) => {
+  return new Promise((resolve, reject) => {
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      },
+      (err, token) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(token);
+        }
+      }
+    );
+  });
+};
+
+module.exports = {
+    generateJWT,
+};
