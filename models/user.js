d2d23111cca1ec132fa2ee30d5e66ec1449150d0
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-01-28 14:44:55 -0300

    add user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const {Schema, model} = require('mongoose');
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+UserSchema.methods.toJSON = function() {
+    const {_v, ...user } = this.toObject();
+    return user;
+}
+
+module.exports = model('User', UserSchema);
